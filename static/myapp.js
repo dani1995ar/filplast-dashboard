c348819c63f0ddaef4e2d79770912cd65a92aa5d
Daniel <dani1995ar@hotmail.com>
2021-11-11 15:46:58 -0500

    added a button to dynamically add more inputs to the create order page

--- static/myapp.js
+++ static/myapp.js
@@ -10,4 +10,52 @@ window.onload = function() {
       });
     }
   });
+
+  const maxInputs = 9; // Max amount of order items per order (i.e. pla azul, abs negro, tpu naranja...)
+  let orderItemsWrapper = $('#orderItemsInputWrapper'); // Order item inputs div
+  let addItemsButton = $('#addProductAndAmount'); // "+" next to amount input
+  let products = document.getElementById('product-id').innerHTML;
+
+  let orderItemsCount = orderItemsWrapper.length; // Amount of order items input div
+  let fieldCount = 1; // Track of total added fields
+  let htmlProductAndAmount = 
+  '<div class="moreItemsWrapper">' +
+    '<div class="col">' +
+    '<div class="form-group">' +
+      '<label for="product-id">Select product name:</label>' +
+      '<select name="product-id" id="product-id" required>' +
+        products +
+      '</select>' +
+    '</div>' +
+  '</div>' +
+  '<div class="col">' +
+    '<div class="form-group">' +
+      '<label for="quantity">Amount of product:</label>' +
+      '<input type="number" placeholder="quantity" name="quantity" min="1" required>' +
+      '<button type="button" name="removeProductAndAmount" id="removeProductAndAmount" class="btn btn-success">-</button>' +
+    '</div>' +
+  '</div>' +
+  '</div>';
+  $(addItemsButton).click(function(e) {
+    if(orderItemsCount <= maxInputs){
+      fieldCount++;
+      $(orderItemsWrapper).append(htmlProductAndAmount);
+      orderItemsCount++;
+    }
+    else {
+      alert('You\'ve reached the maximum amount of fields you can have, contact the developer for more info');
+    }
+    return false;
+  })
+
+  // Remove fields function
+  $("body").on("click","#removeProductAndAmount", function(e) { 
+    if( orderItemsCount > 1 ) {
+            // Remove inputs
+            $(this).closest('.moreItemsWrapper').remove();
+            // Decrement count
+            orderItemsCount--;
+    }
+    return false;
+  })
 }
